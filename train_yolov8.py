2d47a37a16f812ef5e241c50dc028886fcb8b9d1
bencsikb <bencsik.blanka@gmail.com>
2025-02-27 11:41:37 +0100

    train_yolo script moved

--- train_yolov8.py
+++
@@ -1,8 +0,0 @@
-from ultralytics import YOLO
-
-
-if __name__ == "__main__":
-
-    model = YOLO("yolov8x.pt")
-
-    results = model.train(data="config/data/kitti.yaml", epochs=100, imgsz=640, batch=8)
