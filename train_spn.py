bd5fdacbc7e26b2fbbfa89792a5a0d49dbc4f75f
bencsikb <bencsik.blanka@gmail.com>
2025-03-28 18:23:49 +0100

    run_name generation added

--- train_spn.py
+++ train_spn.py
@@ -1,14 +1,22 @@
 import os 
 import argparse
+import datetime
+import uuid
 
 from utils.config_parser import ConfigParser
 from src.model.spn_handler import SPNHandler
 from state_predictor.dataloader import create_pruning_dataloader
 
+def generate_run_name(prefix=""):
+    date_str = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+    short_hash = uuid.uuid4().hex[:6]
+    name = f"{date_str}_{short_hash}_{prefix}" if prefix else f"{date_str}_{short_hash}"
+    return name
+
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('name', type=str) #TODO make it optional
+    parser.add_argument('--fantasy_name', type=str) 
     parser.add_argument('--device', default='')
     args = parser.parse_args()
 
@@ -16,15 +24,9 @@ if __name__ == "__main__":
     conf = ConfigParser.read("config/spn.ini")
 
     # Create logging directory
-    log_dir = os.path.join(conf.save.root, args.name)
-
-    try:
-        if os.path.exists(log_dir):
-            raise FileExistsError(f"Folder already exists: {log_dir}")
-        os.makedirs(log_dir)
-    except FileExistsError as e:
-        print(e)
-
+    run_name = generate_run_name(args.fantasy_name)
+    log_dir = os.path.join(conf.save.root, run_name)
+    os.makedirs(log_dir)
 
     # Save config
     ConfigParser.save(conf, os.path.join(log_dir, "settings.ini"))
