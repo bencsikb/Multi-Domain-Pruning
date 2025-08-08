948db31de1d726d478e89c41746b9d693c4ab912
bencsikb <bencsik.blanka@gmail.com>
2025-08-08 15:13:20 +0200

    new hparams added to optuna

--- optimize_spn.py
+++ optimize_spn.py
@@ -10,34 +10,51 @@ from train_spn import generate_run_name
 
 
 def objective(trial):
-    # Suggest hyperparameters
-    epochs = trial.suggest_int('epochs', 200, 2000, step=100)
-    batch_size = trial.suggest_categorical('batch_size', [128, 256, 512, 1024, 2048, 4096, 8192, 16384])
-
+    # Suggest training hyperparameters
+    epochs = trial.suggest_int('epochs', 300, 1500, step=100)
+    batch_size = trial.suggest_categorical('batch_size', [128, 256, 512, 1024, 2048])
     optimizer_type = trial.suggest_categorical('optimizer', ['adam', 'adamw'])
+    loss = trial.suggest_categorical('loss', ['l1', 'mse', 'logcosh'])
+
     start_lr = trial.suggest_categorical(
         'start_lr', [c * 10**-e for e in range(2, 7) for c in range(1, 10)]
     )
     weight_decay = trial.suggest_categorical(
         'weight_decay', [c * 10**-e for e in range(2, 7) for c in range(1, 10)]
     )
-    momentum = trial.suggest_uniform('momentum', 0.5, 0.99) if optimizer_type == 'sgd' else None
-    #spars_loss_weight = trial.suggest_float('spars_loss_weight', 0.1, 1.0, step=0.1)
-    dmap_loss_weight = trial.suggest_categorical('dmap_loss_weight', [0.1, 1.0, 10.0, 100.0])
+    #momentum = trial.suggest_float('momentum', 0.5, 0.99) if optimizer_type == 'sgd' else None
+    #dmap_loss_weight = trial.suggest_categorical('dmap_loss_weight', [0.1, 1.0, 10.0, 100.0])
 
+    # Suggest Transformer-specific hyperparameters
+    model_dim = trial.suggest_categorical('model_dim', [64, 128, 256])
+    num_heads = trial.suggest_categorical('num_heads', [2, 4, 8])
+    if model_dim % num_heads != 0:
+        raise optuna.exceptions.TrialPruned()
 
-    # Read config and set hyperparameters
-    conf = ConfigParser.read("config/spn.ini")
+    num_layers = trial.suggest_int('num_layers', 1, 6)
+    dropout = trial.suggest_float('dropout', 0.0, 0.3, step=0.05)
+
+    # Read and modify config
+    conf = ConfigParser.read("config/spn_transformer.ini")
     conf.train.epochs = epochs
     conf.train.batch_size = batch_size
     conf.model.optimizer = optimizer_type
     conf.model.start_lr = start_lr
     conf.model.weight_decay = weight_decay
-    if momentum is not None:
-        conf.model.momentum = momentum
+    conf.model.loss = loss
+    #if momentum is not None:
+    #    conf.model.momentum = momentum
+
+    #conf.model.dmap_loss_weight = dmap_loss_weight
+
+    # Set Transformer parameters
+    conf.model.model_dim = model_dim
+    conf.model.num_heads = num_heads
+    conf.model.num_layers = num_layers
+    conf.model.dropout = dropout
 
     # Create logging directory
-    run_name =  generate_run_name("optuna")
+    run_name = generate_run_name("optuna")
     log_dir = os.path.join(conf.save.root, run_name)
     os.makedirs(log_dir, exist_ok=True)
     ConfigParser.save(conf, os.path.join(log_dir, 'settings.ini'))
@@ -49,7 +66,7 @@ def objective(trial):
     train_dataloader = create_pruning_dataloader(conf, split_type='training')
     val_dataloader = create_pruning_dataloader(conf, split_type='validation')
 
-    # Load and train model
+    # Create and train the model
     spn_handler = SPNHandler(conf, log_dir, tb_handler)
     spn_handler.create()
     spn_handler.train(train_dataloader, val_dataloader)
@@ -57,28 +74,30 @@ def objective(trial):
     # Validation
     val_loss, val_metrics = spn_handler.evaluate(val_dataloader)
 
-    # Log each flattened metric to both TensorBoard and Optuna
-    flattened_metric_dict = {}
-    flattened_metric_dict["val_loss"] = val_loss
+    # Log flattened metrics
+    flattened_metric_dict = {"val_loss": val_loss}
     for key, subdict in val_metrics.items():
-            for metric_name, value in subdict.items():
-                metric_full_name = f"{key}.{metric_name}"
-                flattened_metric_dict[metric_full_name] = value
-    
+        for metric_name, value in subdict.items():
+            metric_full_name = f"{key}.{metric_name}"
+            flattened_metric_dict[metric_full_name] = value
+
+    # Log hyperparameters and metrics to TensorBoard
     tb_handler.log_hparams({
         'epochs': epochs,
-        'batch_size': batch_size, 
-        'optimizer': optimizer_type, 
-        'start_lr': start_lr, 
-        'weight_decay': weight_decay, 
-        'momentum': momentum, 
-        #'spars_loss_weight': spars_loss_weight,
-        'dmap_loss_weight': dmap_loss_weight
-    },
-    metric_dict=flattened_metric_dict)
+        'batch_size': batch_size,
+        'optimizer': optimizer_type,
+        'start_lr': start_lr,
+        'weight_decay': weight_decay,
+        #'momentum': momentum,
+        'loss': loss,
+        #'dmap_loss_weight': dmap_loss_weight,
+        'model_dim': model_dim,
+        'num_heads': num_heads,
+        'num_layers': num_layers,
+        'dropout': dropout
+    }, metric_dict=flattened_metric_dict)
 
-
-    return flattened_metric_dict["dmap.mae"] #val_loss
+    return flattened_metric_dict.get("dmap.mae", val_loss)
 
 
 if __name__ == '__main__':
